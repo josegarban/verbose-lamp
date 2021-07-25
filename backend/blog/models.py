6eaeb0beffee8f158d0a5c090e09086e8d1a18ff
unknown <josegarban@gmail.com>
2021-07-25 20:33:22 +0200

    Added backend

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -3,23 +3,15 @@ from datetime import datetime
 from django.template.defaultfilters import slugify
 
 class Categories(models.TextChoices):
-    WORLD = 'world'
-    ENVIRONMENT = 'environment'
-    TECHNOLOGY = 'technology'
-    DESIGN = 'design'
-    CULTURE = 'culture'
-    BUSINESS = 'business'
-    POLITICS = 'politics'
-    OPINION = 'opinion'
-    SCIENCE = 'science'
-    HEALTH = 'health'
-    STYLE = 'style'
-    TRAVEL = 'travel'
+    CAT1 = 'cat1'
+    CAT2 = 'cat2'
+    CAT3 = 'cat3'
+    CAT4 = 'cat4'
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField()
-    category = models.CharField(max_length=50, choices=Categories.choices, default=Categories.WORLD)
+    category = models.CharField(max_length=50, choices=Categories.choices, default=Categories.CAT1)
     thumbnail = models.ImageField(upload_to='photos/%Y/%m/%d/')
     excerpt = models.CharField(max_length=150)
     month = models.CharField(max_length=3)
